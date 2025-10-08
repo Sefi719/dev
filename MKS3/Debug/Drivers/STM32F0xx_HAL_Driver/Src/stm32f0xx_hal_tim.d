adab475b3fb24d5ba1afd89b7022b0418c53bfcc
Sefi719 <240869@vutbr.cz>
2025-10-08 12:02:56 +0200

    Rotary encoder

--- MKS3/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.d
+++ MKS3/Debug/Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.d
@@ -1,4 +1,5 @@
-Core/Src/sct.o: ../Core/Src/sct.c ../Core/Inc/sct.h ../Core/Inc/main.h \
+Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.o: \
+ ../Drivers/STM32F0xx_HAL_Driver/Src/stm32f0xx_hal_tim.c \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h \
  ../Core/Inc/stm32f0xx_hal_conf.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h \
@@ -28,8 +29,6 @@ Core/Src/sct.o: ../Core/Src/sct.c ../Core/Inc/sct.h ../Core/Inc/main.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_tim_ex.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart.h \
  ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_uart_ex.h
-../Core/Inc/sct.h:
-../Core/Inc/main.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal.h:
 ../Core/Inc/stm32f0xx_hal_conf.h:
 ../Drivers/STM32F0xx_HAL_Driver/Inc/stm32f0xx_hal_rcc.h:

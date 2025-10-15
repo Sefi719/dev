134a5334f51baaacb161ed4777aaadf77326b477
Sefi719 <240869@vutbr.cz>
2025-10-15 12:08:21 +0200

    smoothing

--- MKS4/Core/Src/main.c
+++ MKS4/Core/Src/main.c
@@ -31,7 +31,7 @@
 
 /* Private define ------------------------------------------------------------*/
 /* USER CODE BEGIN PD */
-
+#define ADC_Q	12
 /* USER CODE END PD */
 
 /* Private macro -------------------------------------------------------------*/
@@ -297,7 +297,10 @@ static void MX_GPIO_Init(void)
 
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef* hadc)
 {
- raw_pot = HAL_ADC_GetValue(hadc);
+	static uint32_t avg_pot;
+	raw_pot = avg_pot >> ADC_Q;
+	avg_pot -= raw_pot;
+	avg_pot += HAL_ADC_GetValue(hadc);
 }
 /* USER CODE END 4 */
 

66f268c6472be4453a52abfabb6a8672719b8390
Sefi719 <240869@vutbr.cz>
2025-10-15 12:07:27 +0200

    1st

---
+++ MKS4/Core/Inc/sct.h
@@ -0,0 +1,17 @@
+/*
+ * sct.h
+ *
+ *  Created on: Oct 8, 2025
+ *      Author: 240869
+ */
+
+#ifndef INC_SCT_H_
+#define INC_SCT_H_
+
+#include "main.h"
+
+void sct_init(void);
+void sct_led(uint32_t value);
+void sct_value(uint16_t value, uint8_t led);
+
+#endif /* INC_SCT_H_ */
